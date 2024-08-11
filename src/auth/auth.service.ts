429edd417ebe99f50e8795581234d93466fafff7
Anton Zhade <el12guitar@gmail.com>
2024-08-11 11:59:16 +0300

    add auth service logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwt: JwtService,
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.userService.getByIEmail(dto.email);
+
+    if (oldUser) throw new Error('Пользователь с таким email уже зарегист');
+
+    const user = await this.userService.create(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  issueTokens(userId: string) {
+    const data = { id: userId };
+
+    const accessToken = this.jwt.sign(data, { expiresIn: '1h' });
+    const refreshToken = this.jwt.sign(data, { expiresIn: '7d' });
+
+    return { accessToken, refreshToken };
+  }
+
+  private async validateUser(dto: AuthDto): Promise<any> {
+    const user = await this.userService.getByIEmail(dto.email);
+
+    if (!user) throw new NotFoundException('Пользователь не найден');
+
+    return user;
+  }
+}

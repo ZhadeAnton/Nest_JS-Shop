998598fa7e945702b32be89e6bd1132e043865a5
Anton Zhade <el12guitar@gmail.com>
2024-08-14 11:57:02 +0300

    add file module

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  Query,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileService } from './file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@Controller('file')
+@Controller('files')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
+
+  @HttpCode(200)
+  @UseInterceptors(FileInterceptor('files'))
+  @Auth()
+  @Post()
+  async saveFiles(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Query() folder?: string,
+  ) {
+    return this.fileService.saveFile(files, folder);
+  }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+  async saveFile(files: Express.Multer.File[], folder: string = 'products') {
+    const uploadedFolder = `${path}/quploads/${folder}`;
+
+    await ensureDir(uploadedFolder);
+
+    const response = Promise.all(
+      files.map(async (file) => {
+        const originalName = `${Date.now()}-${file.originalname}`;
+        
+        await writeFile(`${uploadedFolder}/${originalName}`, file.buffer);
+
+        return {
+          url: `/uploads/${folder}/${originalName}`,
+          name: originalName,
+        };
+      }),
+    );
+
+    return response;
+  }
+}
